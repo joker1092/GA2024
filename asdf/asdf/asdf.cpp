e8358724331c3778e84f99fbe9040a7e2c3cc19d
joker1092 <joker1092@nate.com>
2024-03-20 19:02:57 +0900

    0320commit

--- asdf/asdf/asdf.cpp
+++ asdf/asdf/asdf.cpp
@@ -3,25 +3,9 @@ using namespace std;
 
 int main(void)
 {
-	int A,B = 0;
-	cin >> A;
-	cin >> B;
-	if (A > 0 && B > 0) {
-		cout << 1 << endl;
-	}
-	else if (A > 0 && B < 0) {
-		cout << 4 << endl;
-	}
-	else if (A < 0 && B < 0)
-	{
-		cout << 3 << endl;
-	}
-	else if (A < 0 && B > 0) {
-		cout << 2 << endl;
-	}
-	else {
-		cout << 0 << endl;
-	}
+	int N = 0, M = 0;
+
 	
+
 	return 0;
 }
