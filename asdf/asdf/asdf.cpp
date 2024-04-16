f3912d260e1fdce1f53ca0447d997dd91e2a3aee
joker1092 <joker1092@nate.com>
2024-04-16 18:13:16 +0900

    0416 commit

--- asdf/asdf/asdf.cpp
+++ asdf/asdf/asdf.cpp
@@ -3,23 +3,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	const int count = 10;
-
-	//int* p = new int[count];
-	int* p = (int*)malloc(sizeof(int) * count);
-
-	for (int i = 0; i < count; i++)
-	{
-		*(p + i)=i+1;
-	}
-
-	for (int i = 0; i < count; i++)
-	{
-		cout << *(p + i) << endl;
-	}
 	
-	//delete[] p;
-	free(p);
 
 	return 0;
 }
