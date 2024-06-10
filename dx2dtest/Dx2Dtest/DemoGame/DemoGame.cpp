7a27a7332a4efabe38486ed2fa2b8f321ca7a5b2
joker1092 <74646723+joker1092@users.noreply.github.com>
2024-06-10 17:08:41 +0900

    0610 commit

---
+++ dx2dtest/Dx2Dtest/DemoGame/DemoGame.cpp
@@ -0,0 +1,24 @@
+﻿#include "../Dx2Dtest/WinGameApp.h"
+
+class DemoGame : public WinGameApp
+{
+public:
+	DemoGame ();
+	~DemoGame ();
+
+private:
+
+};
+
+DemoGame::DemoGame()
+{
+
+}
+
+DemoGame::~DemoGame()
+{
+}
+int main()
+{
+    
+}
