5508ffa6a640cdedc856dcd932f75d7f08d6a934
joker1092 <74646723+joker1092@users.noreply.github.com>
2024-12-17 00:48:48 +0900

    1216 house

--- DX12Project1/DX12Project1/Device.cpp
+++ DX12Project1/DX12Project1/Device.cpp
@@ -20,8 +20,35 @@ ID3D12Resource* g_pRT[g_RTCnt] = { NULL, };
 #define g_BackBuffer g_pBB
 //#define g_RTViews g_pRTDesc
 
+//명령어큐 인터페이스들
+ID3D12CommandQueue*         g_pCommandQueue = NULL;
+ID3D12CommandAllocator*     g_pCommandAllocator = NULL;
+ID3D12GraphicsCommandList*  g_pCommandList = NULL;
 
+//렌더링 동기화 인터페이스
+ID3D12Fence* g_pFence = NULL;
+UINT64 g_FenceValue = 0;
+//UINT64 g_FenceValues[g_RTCnt] = { 0, };
+//UINT g_FrameIndex = 0;
+HANDLE g_FenceEvent = NULL;
 
+//뷰포트
+D3D12_VIEWPORT g_VP;
+
+//장치 설정 기본 정보
+DISPLAYMODE g_Mode = { 960,600,0,1,DXGI_FORMAT_R8G8B8A8_UNORM };
+
+//AA & AF Option
+DWORD g_dwAA = 1;
+DWORD g_dwAF = 1;
+BOOL g_bMipMap = TRUE;
+
+//D3D 기능 레벨
+D3D_FEATURE_LEVEL g_FeatLv = D3D_FEATURE_LEVEL_12_0; //DX12
+//D3D_FEATURE_LEVEL g_FeatLv = D3D_FEATURE_LEVEL_12_1; //DX12.1
+//D3D_FEATURE_LEVEL g_FeatLv = D3D_FEATURE_LEVEL_11_0; //DX11
+//D3D_FEATURE_LEVEL g_FeatLv = D3D_FEATURE_LEVEL_9_3; //DX9.0c
+const TCHAR* g_strFeatLv = _T("N/A");
 
 int DXSetup(HWND hwnd)
 {
